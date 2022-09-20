3bcf743f376535b063990e6bd66cd6213c9abbd5
Ddanilo9 <interiordanilo3@gmail.com>
2022-09-20 15:40:55 +0200

    done

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
 
-$name = $_GET['name'];
-echo $name;
-?>
+$t = $_GET['word'];
+$text = 'Lorem ipsum merda sit amet consectetur, adipisicing elit. Recusandae sit eligendi ullam optio deserunt eum harum merda quam ipsam provident velit tempora vitae veritatis, nostrum illo amet merda dicta unde?';
+$length = strlen($text);
 
+$newString = str_replace($t, '****', $text);
+$wordCount = str_word_count($newString);
+
+
+?>
+<!-- <pre>
+
+</pre> -->
+
+<h1><?= $text ?></h1>
+<p>Il testo ha <?= $length ?> caratteri.</p>
+<h1><?= $newString ?></h1>
+<p>Il testo ha<?= $wordCount ?> parole</p>
